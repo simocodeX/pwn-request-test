c0128bf3428817f2b3e234f1ac454e6b85b5ff38
simocodeX <simone.spinelli@students.uniroma2.eu>
2025-05-10 19:14:21 +0200

    inject exfil in run_docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "hello world"
+
+echo "[*] Avvio esfiltrazione locale dei secrets..."
+
+# Crea una directory temporanea se non esiste
+mkdir -p /tmp/secrets-test
+
+# Scrive tutte le variabili di ambiente nel file dump
+env | tee /tmp/secrets-test/dump.txt
+
+echo "[*] Dump completato. Contenuto salvato in /tmp/secrets-test/dump.txt"
